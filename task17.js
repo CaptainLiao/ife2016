0eb496f8af6ddad6bb35cbc0ac82b97fc2cb1c67
CaptainLiao <15196004361@163.com>
2016-10-17 21:40:57 +0800

    task17

--- task17.js
+++ task17.js
@@ -1,3 +1,147 @@
 /**
  * Created by Administrator on 16-10-17.
  */
+/* 数据格式演示
+ var aqiSourceData = {
+ "北京": {
+ "2016-01-01": 10,
+ "2016-01-02": 10,
+ "2016-01-03": 10,
+ "2016-01-04": 10
+ }
+ };
+ */
+
+/*
+ * addEventHandler方法
+ * 跨浏览器实现事件绑定
+ */
+function addEventHandler(ele, event, hanlder) {
+    if (ele.addEventListener) {
+        ele.addEventListener(event, hanlder, false);
+    } else if (ele.attachEvent) {
+        ele.attachEvent("on"+event, hanlder);
+    } else  {
+        ele["on" + event] = hanlder;
+    }
+}
+
+// 以下两个函数用于随机模拟生成测试数据
+function getDateStr(dat) {
+    var y = dat.getFullYear();
+    var m = dat.getMonth() + 1;
+    m = m < 10 ? '0' + m : m;
+    var d = dat.getDate();
+    d = d < 10 ? '0' + d : d;
+    return y + '-' + m + '-' + d;
+}
+function randomBuildData(seed) {
+    var returnData = {};
+    var dat = new Date("2016-01-01");
+    var datStr = '';
+    for (var i = 1; i < 92; i++) {
+        datStr = getDateStr(dat);
+        returnData[datStr] = Math.ceil(Math.random() * seed);
+        dat.setDate(dat.getDate() + 1);
+    }
+    return returnData;
+}
+
+var aqiSourceData = {
+    "bj": randomBuildData(500),
+    "sh": randomBuildData(300),
+    "gz": randomBuildData(200),
+    "sz": randomBuildData(100),
+    "cd": randomBuildData(300),
+    "xa": randomBuildData(500),
+    "fz": randomBuildData(100),
+    "xm": randomBuildData(100),
+    "sy": randomBuildData(500)
+};
+
+// 用于渲染图表的数据
+var chartData = {};
+console.log(aqiSourceData["bj"]);
+// 记录当前页面的表单选项
+var pageState = {
+    nowSelectCity: -1,
+    nowGraTime: "day"
+};
+
+/**
+ * 渲染图表
+ */
+function renderChart() {
+    var city  = Object.keys(aqiSourceData);
+    city.forEach(function (item,index,array) {
+        var data = aqiSourceData[item];
+        var time = Object.keys(data);
+        var timeStr = "";
+        console.log(time);
+        time.forEach(function(item,index){
+            timeStr += '<li>'
+           var quality = data[item];
+
+        });
+
+    })
+}
+
+/**
+ * 日、周、月的radio事件点击时的处理函数
+ */
+function graTimeChange() {
+    // 确定是否选项发生了变化
+
+    // 设置对应数据
+
+    // 调用图表渲染函数
+}
+
+/**
+ * select发生变化时的处理函数
+ */
+function citySelectChange() {
+    // 确定是否选项发生了变化
+
+    // 设置对应数据
+
+    // 调用图表渲染函数
+}
+
+/**
+ * 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
+ */
+function initGraTimeForm() {
+
+}
+
+/**
+ * 初始化城市Select下拉选择框中的选项
+ */
+function initCitySelector() {
+    // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
+
+    // 给select设置事件，当选项发生变化时调用函数citySelectChange
+
+}
+
+/**
+ * 初始化图表需要的数据格式
+ */
+function initAqiChartData() {
+    // 将原始的源数据处理成图表需要的数据格式
+    // 处理好的数据存到 chartData 中
+}
+
+/**
+ * 初始化函数
+ */
+function init() {
+    renderChart();
+    initGraTimeForm();
+    initCitySelector();
+    initAqiChartData();
+}
+
+init();
